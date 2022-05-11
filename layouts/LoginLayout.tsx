428f357772cae8f73dace5c054aa27e5fc1a5680
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-10 22:17:34 -0400

    clean: split layout from login page

---
+++ layouts/LoginLayout.tsx
@@ -0,0 +1,26 @@
+import { Container, Typography } from "@mui/material";
+import Head from "next/head";
+import { FC, PropsWithChildren } from "react";
+
+export type LoginLayoutProps = PropsWithChildren<{ title: string }>;
+
+export const LoginLayout: FC<LoginLayoutProps> = ({ children, title }) => (
+  <>
+    <Head>
+      <title>{title} - MyMovies</title>
+    </Head>
+    <Container
+      sx={{
+        p: 4,
+        minWidth: "100vw",
+        minHeight: "100vh",
+        bgcolor: (theme) => theme.palette.primary.light,
+      }}
+    >
+      <Typography component="h1" variant="h3" align="center" gutterBottom>
+        {title}
+      </Typography>
+      {children}
+    </Container>
+  </>
+);
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,12 @@
-import { Container, Typography } from "@mui/material";
 import { NextPage } from "next";
-
-import Head from "next/head";
 import LoginForm from "../components/LoginForm";
+import { LoginLayout } from "../layouts/LoginLayout";
 
 const LoginPage: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Login - MyMovies</title>
-      </Head>
-      <Container sx={{ p: 4 }}>
-        <Typography component="h1" variant="h3" align="center" gutterBottom>
-          Login
-        </Typography>
-        <LoginForm />
-      </Container>
-    </>
+    <LoginLayout title="Login">
+      <LoginForm />
+    </LoginLayout>
   );
 };
 
