dbccfc0adcab34eea6137a35f1171ec12daec265
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-12 08:59:20 -0400

    feat: user signup

---
+++ pages/api/user/signup.ts
@@ -0,0 +1,76 @@
+import { User } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { prisma } from "config/db";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+export const signUpUserRequestSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+  birthday: z
+    .union([z.string(), z.date()])
+    .transform((input) => {
+      if (typeof input === "string") {
+        return new Date(input);
+      }
+      return input;
+    })
+    .refine((input) => input < new Date(), "Birthday needs to be before today"),
+});
+
+export type SignUpUserRequest = z.infer<typeof signUpUserRequestSchema>;
+
+async function SignUpRoute(req: NextApiRequest, res: NextApiResponse) {
+  const body = req.body;
+  try {
+    const request = signUpUserRequestSchema.parse(body);
+    const user = await prisma.user.findUnique({
+      where: { email: request.email },
+      include: { credential: true },
+    });
+    if (!user) {
+      await registerNewUser(request);
+      res.status(201).end();
+    } else {
+      if (user.credential) {
+        res.status(403).end();
+        return;
+      }
+      await createUserInfo(user, request);
+      return;
+    }
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      console.log(err);
+      res.status(400).json({ message: err.message });
+      return;
+    }
+    throw err;
+  }
+}
+
+export default SignUpRoute;
+
+async function registerNewUser(request: SignUpUserRequest) {
+  const user = await prisma.user.create({
+    data: { email: request.email },
+  });
+  await createUserInfo(user, request);
+}
+
+async function createUserInfo(user: User, request: SignUpUserRequest) {
+  await createUserData(user, request);
+  await createUserCredential(user, request.password);
+}
+
+const SALT_ROUNDS = 10;
+async function createUserData(user: User, request: SignUpUserRequest) {
+  await prisma.userData.create({
+    data: { birthday: request.birthday, userId: user.id },
+  });
+}
+
+async function createUserCredential(user: User, rawPassword: string) {
+  const password = await bcrypt.hash(rawPassword, SALT_ROUNDS);
+  await prisma.credential.create({ data: { password, userId: user.id } });
+}
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,32 @@
 import { Button, Paper, TextField } from "@mui/material";
+import axios from "axios";
 import { LoginLayout } from "layouts/LoginLayout";
+import { useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const SignUpPage = () => {
+  const [signingUp, setSigningUp] = useState<boolean>(false);
+
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  const signUp = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (signingUp) return;
+    setSigningUp(true);
+    const formData = new FormData(formRef.current!);
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      birthday: formData.get("birthday"),
+    };
+    try {
+      await axios.post("/api/user/signup", data);
+    } catch (err) {
+      alert("Failed to sign up. Try again later.");
+    }
+    setSigningUp(false);
+  };
+
   return (
     <LoginLayout title="Sign Up">
       <Paper
@@ -13,6 +38,8 @@ const SignUpPage = () => {
           maxWidth: "sm",
           width: "100%",
         }}
+        ref={formRef}
+        onSubmit={signUp}
       >
         <TextField name="email" type="email" label="Email" margin="dense" />
         <TextField
@@ -27,7 +54,12 @@ const SignUpPage = () => {
           label="Birthday"
           margin="dense"
         />
-        <Button variant="contained" sx={{ mt: 2 }}>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ mt: 2 }}
+          disabled={signingUp}
+        >
           Sign Up
         </Button>
       </Paper>
