78140fa61e287323aa29630c566944da36947cec
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-13 09:27:24 -0400

    feat: profile creation

---
+++ pages/api/user/profiles.ts
@@ -0,0 +1,71 @@
+import { User, UserData } from "@prisma/client";
+import { prisma } from "config/db";
+import { withAuthentication } from "middlewares/backend/withAuthentication";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z, ZodError } from "zod";
+
+// This could go to a config file of business rules
+const MAX_PROFILE_COUNT_PER_USER = 4;
+
+export const zodCreateProfileRequest = z.object({
+  name: z.string().min(1),
+});
+
+export type CreateProfileRequest = z.infer<typeof zodCreateProfileRequest>;
+
+async function CreateProfileHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user: User & { info: UserData }
+) {
+  const { name } = zodCreateProfileRequest.parse(req.body);
+  const profileCount = await prisma.profile.count({
+    where: { userDataId: user.info.id },
+  });
+  if (profileCount + 1 > MAX_PROFILE_COUNT_PER_USER) {
+    res
+      .status(403)
+      .json({ error: { message: "Only 4 profiles per user are allowed" } });
+    return;
+  }
+  const nameIsUsed = !!(await prisma.profile.findFirst({
+    where: { userDataId: user.info.id, name },
+  }));
+  if (nameIsUsed) {
+    res.status(403).json({
+      error: { message: "A profile with the same name already exists" },
+    });
+    return;
+  }
+  await prisma.profile.create({ data: { userDataId: user.info.id, name } });
+  res.status(201).end();
+}
+
+async function ProfileRoute(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user: User & { info: UserData }
+) {
+  try {
+    switch (req.method) {
+      case "POST":
+        await CreateProfileHandler(req, res, user);
+        return;
+      default:
+        res.status(405).json({
+          error: {
+            message: `This route does not allow ${req.method} requests`,
+          },
+        });
+        return;
+    }
+  } catch (err) {
+    if (err instanceof ZodError) {
+      res.status(400).json({ error: { message: err.message } });
+      return;
+    }
+    throw err;
+  }
+}
+
+export default withAuthentication(ProfileRoute);
