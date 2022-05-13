cea07f5c9f8b8efcfb1bb4a2fb1aa818cfdb8709
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-12 22:40:04 -0400

    feat: add query param to get movies route

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,12 +1,19 @@
 import movieDB from "apis/tmdb";
 import { NextApiHandler } from "next";
 
+export type GetMoviesQuery = {
+  q?: string;
+};
+
 export type GetMoviesResponse = {
   movieIds: number[];
 };
 
 const GetMoviesRoute: NextApiHandler<GetMoviesResponse> = async (req, res) => {
-  const tmdbResponse = await movieDB.moviePopular();
+  const { q: query } = req.query as GetMoviesQuery;
+  const tmdbResponse = await (query
+    ? movieDB.searchMovie({ query })
+    : movieDB.moviePopular());
   const movies = tmdbResponse.results!;
   const movieIds = movies.map(({ id }) => id!);
   res.json({ movieIds });
