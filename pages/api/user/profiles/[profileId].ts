8e0334adfb7180767203d569afd54ba990039923
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-14 11:26:20 -0400

    feat: get specific profile route

---
+++ pages/api/user/profiles/[profileId].ts
@@ -0,0 +1,34 @@
+import { User, UserData } from "@prisma/client";
+import { prisma } from "config/db";
+import { convertProfileModelToDTO, ProfileDTO } from "dtos/Profile";
+import { withAuthentication } from "middlewares/backend/withAuthentication";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export type GetProfileResponse = {
+  profile: ProfileDTO;
+};
+
+async function GetProfileRoute(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user: User & { info: UserData }
+) {
+  const profileId = Array.isArray(req.query.profileId)
+    ? req.query.profileId[0]
+    : req.query.profileId;
+  const profile = await prisma.profile.findFirst({
+    where: { id: profileId, userDataId: user.info.id },
+    include: { movieWatchlist: true },
+  });
+  if (!profile) {
+    res.status(404).json({
+      error: { message: "User has no profile with the specified ID" },
+    });
+    return;
+  }
+  const dto = convertProfileModelToDTO(profile);
+  const resBody: GetProfileResponse = { profile: dto };
+  res.json(resBody);
+}
+
+export default withAuthentication(GetProfileRoute);
