deb6873ba8dde83d7285ee31eeea0ae6e5d60afc
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-14 17:38:40 -0400

    feat: add to watchlist

---
+++ pages/api/user/profiles/[profileId]/watchlist.ts
@@ -0,0 +1,89 @@
+import { MovieWatchlistEntry, Profile, User, UserData } from "@prisma/client";
+import movieDB from "apis/tmdb";
+import { prisma } from "config/db";
+import { withAuthentication } from "middlewares/backend/withAuthentication";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import firstOrSelf from "utils/firstOrSelf";
+import { z, ZodError } from "zod";
+
+export const zodAddMovieToWatchlistRequest = z.object({ movieId: z.number() });
+
+export type AddMovieToWatchlistRequest = z.infer<
+  typeof zodAddMovieToWatchlistRequest
+>;
+
+async function AddMovieToWatchlistHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  profile: Profile
+) {
+  const { movieId } = zodAddMovieToWatchlistRequest.parse(req.body);
+  const movieExists = await movieDB
+    .movieInfo({ id: movieId })
+    .then(() => true)
+    .catch(() => false);
+  if (!movieExists) {
+    return res
+      .status(404)
+      .json({ error: { message: "The specified movie does not exist" } });
+  }
+  const alreadyOnTheList = !!(await prisma.movieWatchlistEntry.findUnique({
+    where: {
+      movieId_profileId: { movieId: movieId.toString(), profileId: profile.id },
+    },
+  }));
+  if (alreadyOnTheList) return res.status(200).end();
+  await prisma.movieWatchlistEntry.create({
+    data: {
+      profileId: profile.id,
+      movieId: movieId.toString(),
+      watched: false,
+    },
+  });
+  return res.status(201).end();
+}
+
+async function ProfileWatchlistRoute(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  user: User & { info: UserData }
+) {
+  const profileId = firstOrSelf(req.query.profileId)!;
+  const profile = await prisma.profile.findUnique({
+    where: { id: profileId },
+    include: { userData: true },
+  });
+  if (!profile) {
+    res
+      .status(404)
+      .json({ error: { message: "The specified profile does not exist" } });
+    return;
+  }
+  if (profile.userDataId !== user.info.id) {
+    res
+      .status(403)
+      .json({ error: { message: "Profile belongs to another user" } });
+    return;
+  }
+  try {
+    switch (req.method) {
+      case "POST":
+        await AddMovieToWatchlistHandler(req, res, profile);
+        return;
+      default:
+        return res.status(405).json({
+          error: {
+            message: `This route does not allow ${req.method} requests`,
+          },
+        });
+    }
+  } catch (err) {
+    if (err instanceof ZodError) {
+      res.status(400).json({ error: { message: err.message } });
+      return;
+    }
+    throw err;
+  }
+}
+
+export default withAuthentication(ProfileWatchlistRoute);
