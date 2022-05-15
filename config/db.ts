f955520c18965502b70d2d278847892f8d6795b3
Bruno Henrique Glowaski Morais <fluffybucketsnake@outlook.com>
2022-05-15 15:19:06 -0400

    fix: too many prisma connections

--- config/db.ts
+++ config/db.ts
@@ -1,3 +1,4 @@
 import { PrismaClient } from "@prisma/client";
+import { registerService } from "utils/registerService";
 
-export const prisma = new PrismaClient();
+export const prisma = registerService("prisma", () => new PrismaClient());
---
+++ utils/registerService.ts
@@ -0,0 +1,12 @@
+// Taken from https://dev.to/noclat/fixing-too-many-connections-errors-with-database-clients-stacking-in-dev-mode-with-next-js-3kpm
+export const registerService = <T>(name: string, initFn: () => T) => {
+  if (process.env.NODE_ENV === "development") {
+    if (!(name in globalThis)) {
+      // @ts-ignore
+      globalThis[name] = initFn();
+    }
+    // @ts-ignore
+    return globalThis[name];
+  }
+  return initFn();
+};
